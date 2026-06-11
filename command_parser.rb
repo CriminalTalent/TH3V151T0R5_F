864d55d4b97c6a370530f285c4f75c1efa3ac9bd
CriminalTalent <rntjdrud@naver.com>
2026-06-11 12:06:48 +0900

    Create command_parser.rb

---
+++ command_parser.rb
@@ -0,0 +1,48 @@
+# command_parser.rb
+# encoding: UTF-8
+require 'cgi'
+
+require_relative 'commands/location_command'
+require_relative 'commands/scout_command'
+require_relative 'commands/investigate_command'
+
+module CommandParser
+  def self.parse(mastodon_client, sheet_manager, notification)
+    content_raw = notification.dig('status', 'content') || ''
+    sender      = notification.dig('account', 'acct') || ''
+    content     = clean_html(content_raw)
+    status      = notification['status']
+
+    puts "[PARSER] @#{sender}: #{content}"
+
+    case content
+    when /\[위치\/(.+?)\]/
+      LocationCommand.new(sheet_manager, mastodon_client, sender, $1.strip, status).execute
+
+    when /\[둘러보기\]/
+      ScoutCommand.new(sheet_manager, mastodon_client, sender, status).execute
+
+    when /\[조사\/(.+?)\]/
+      InvestigateCommand.new(sheet_manager, mastodon_client, sender, $1.strip, status).execute
+
+    else
+      return
+    end
+
+  rescue => e
+    puts "[PARSER 오류] #{e.class}: #{e.message}"
+  end
+
+  def self.clean_html(html)
+    return '' if html.nil?
+    CGI.unescapeHTML(
+      html.to_s
+        .gsub(/<br\s*\/?>/i, "\n")
+        .gsub(/<\/p\s*>/i, "\n")
+        .gsub(/<p[^>]*>/i, '')
+        .gsub(/<[^>]*>/, '')
+    ).gsub("\u00A0", ' ').strip
+  rescue
+    html.to_s
+  end
+end
